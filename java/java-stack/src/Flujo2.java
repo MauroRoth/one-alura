6680ab81f5bb458751e978ce5c4be17f5ff5af8d
Mauro Roth <maurogroth@gmail.com>
2023-03-02 18:16:47 -0300

    comenzando a ver manejo de excepciones

---
+++ java/java-stack/src/Flujo2.java
@@ -0,0 +1,19 @@
+
+public class Flujo2 {
+
+    public static void main(String[] args) {
+
+        String nombre = "Alura"; 
+        System.out.println("ANTES");
+
+        try { 
+            System.out.println(nombre);
+        } catch(ArrayIndexOutOfBoundsException ex) { 
+            System.out.println("CATCH");
+        }
+
+        System.out.println("DESPUES"); 
+    }
+
+} 
+
